ee05ce41d8df6b2b6950d10e2f1da97809149d49
Axel D <axel@spliiit.com>
2025-06-25 15:54:38 +0200

    update i18n locale

--- i18n/index.js
+++ i18n/index.js
@@ -3,9 +3,10 @@ import fr from './locales/fr.json';
 import en from './locales/en.json';
 
 export default createI18n({
-    locale: import.meta.env.VITE_DEFAULT_LOCALE,
-    fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
+    locale: 'fr',
+    fallbackLocale: 'en',
     legacy: false,
+    globalInjection: true,
     messages: {
         fr,
         en

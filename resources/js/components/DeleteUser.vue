3dbc9b4fc0d90aef5c3bb8f15900aef6fcec6b88
Axel D <axel@spliiit.com>
2025-07-03 10:47:25 +0200

    feat(delete-account): update modal and layout

--- resources/js/components/DeleteUser.vue
+++ resources/js/components/DeleteUser.vue
@@ -16,14 +16,16 @@ import {
     DialogTitle,
     DialogTrigger,
 } from '@/components/ui/dialog';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { useI18n } from 'vue-i18n';
+import FormInput from './burnout-ui/input/FormInput.vue';
 
 const passwordInput = ref<HTMLInputElement | null>(null);
 
 const form = useForm({
     password: '',
 });
+
+const { t } = useI18n();
 
 const deleteUser = (e: Event) => {
     e.preventDefault();
@@ -44,38 +46,44 @@ const closeModal = () => {
 
 <template>
     <div class="space-y-6">
-        <HeadingSmall title="Delete account" description="Delete your account and all of its resources" />
+        <HeadingSmall :title="t('profile.delete-account.title')" :description="t('profile.delete-account.description')" />
         <div class="space-y-4 rounded-lg border border-red-100 bg-red-50 p-4 dark:border-red-200/10 dark:bg-red-700/10">
             <div class="relative space-y-0.5 text-red-600 dark:text-red-100">
-                <p class="font-medium">Warning</p>
-                <p class="text-sm">Please proceed with caution, this cannot be undone.</p>
+                <p class="font-medium">{{ t('profile.delete-account.warning') }}</p>
+                <p class="text-sm">{{ t('profile.delete-account.before-delete') }}</p>
             </div>
             <Dialog>
                 <DialogTrigger as-child>
-                    <Button variant="destructive">Delete account</Button>
+                    <Button variant="destructive">{{ t('profile.delete-account.title') }}</Button>
                 </DialogTrigger>
                 <DialogContent>
                     <form class="space-y-6" @submit="deleteUser">
                         <DialogHeader class="space-y-3">
-                            <DialogTitle>Are you sure you want to delete your account?</DialogTitle>
+                            <DialogTitle>{{ t('profile.delete-account.confirm.title') }}</DialogTitle>
                             <DialogDescription>
-                                Once your account is deleted, all of its resources and data will also be permanently deleted. Please enter your
-                                password to confirm you would like to permanently delete your account.
+                                {{ t('profile.delete-account.confirm.description') }}
                             </DialogDescription>
                         </DialogHeader>
 
                         <div class="grid gap-2">
-                            <Label for="password" class="sr-only">Password</Label>
-                            <Input id="password" type="password" name="password" ref="passwordInput" v-model="form.password" placeholder="Password" />
-                            <InputError :message="form.errors.password" />
+                            <FormInput
+                                id="password"
+                                type="password"
+                                v-model="form.password"
+                                required
+                                autocomplete="password"
+                                :label="t('profile.delete-account.confirm.password')"
+                                @update:model-value="form.password = $event"
+                            />
+                            <InputError v-if="form.errors.password" class="mt-2" :message="form.errors.password" />
                         </div>
 
                         <DialogFooter class="gap-2">
                             <DialogClose as-child>
-                                <Button variant="secondary" @click="closeModal"> Cancel </Button>
+                                <Button class="cursor-pointer" variant="secondary" @click="closeModal">{{ t('profile.delete-account.confirm.cancel') }}</Button>
                             </DialogClose>
 
-                            <Button type="submit" variant="destructive" :disabled="form.processing"> Delete account </Button>
+                            <Button type="submit" class="cursor-pointer" variant="destructive" :disabled="form.processing">{{ t('profile.delete-account.title') }}</Button>
                         </DialogFooter>
                     </form>
                 </DialogContent>
